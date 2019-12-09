b80245f8a936d80ce08eb66fb10337135431f002
mamaj20 <martynamaj@icloud.com>
2019-12-09 11:49:18 +0000

    added findByName implementation in ToolDao

--- sharedpower/dao/ToolDao.py
+++ sharedpower/dao/ToolDao.py
@@ -1,27 +1,21 @@
 from ..dbconnection.DatabaseHelpers import DatabaseHelpers
 
 class ToolDao:
-    def findByName(self, name):
-        database_connection = DatabaseHelpers.CreateConnection()
+    def findByName(self, toolName):
+        #connecting to the database
+        database_connection = DatabaseHelpers.createConnection()
 
+        #creating a cursor that will allow SQL commands to be executed in python
+        cursor = database_connection.cursor()
 
-        
-        DatabaseHelpers.CloseConnection(database_connection)
+        #creating a query that the cursor can follow in order to retrieve data
+        cursor.execute('SELECT tool_name, is_active, price_per_day, price_per_half_day, fine FROM TOOL WHERE tool_name = ?')
 
+        #retrieving one or more records from the database
+        tool_row = cursor.fetchall()
 
+        #disconnecting from the database
+        DatabaseHelpers.closeConnection(database_connection)
 
-
-
-
-
-
-
-
-
-        #query = 'SELECT username, password FROM USER WHERE username = ?'
-        #database_connection = DatabaseHelpers.createConnection()
-        #cursor = database_connection.cursor()
-        #cursor.execute(query, (username,))
-        #customer_row = cursor.fetchone()
-        #DatabaseHelpers.closeConnection(database_connection)
-        #return customer_row
+        #displaying all the records that are adequate to the tool's name
+        return tool_row
