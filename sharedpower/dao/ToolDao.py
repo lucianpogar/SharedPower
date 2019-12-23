2973b982673437d7001e7ff7c670babfb5bf8e25
mamaj20 <martynamaj@icloud.com>
2019-12-23 18:19:42 +0000

    added numbers adequate to the number of fetched records

--- sharedpower/dao/ToolDao.py
+++ sharedpower/dao/ToolDao.py
@@ -6,7 +6,7 @@ class ToolDao:
         cursor = database_connection.cursor()
         cursor.execute('SELECT tool_name, is_active, price_per_day, price_per_half_day, fine FROM TOOL WHERE tool_name = ?', (tool_name,))
         tool_row = cursor.fetchall()
-        for i in tool_row:
-            print(i)
+        for count, i in enumerate(tool_row, start=1):
+            print(count, ".", i)
         DatabaseHelpers.closeConnection(database_connection)
         return tool_row
