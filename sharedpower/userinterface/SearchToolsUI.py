92f1d2dc33ba0be4ed4228d503317d3144a3de8c
mamaj20 <martynamaj@icloud.com>
2020-01-04 17:25:45 +0000

    deleted switch statement

--- sharedpower/userinterface/SearchToolsUI.py
+++ sharedpower/userinterface/SearchToolsUI.py
@@ -2,19 +2,11 @@ from ..controllers.ToolController import ToolController
 #from ..model.Specification import toolSpecification
 
 class SearchToolsUI :
-    def caseStatement(self, chosen_item):
-        switcher = {
-            "1" : "pass",
-            "2" : "pass",
-        }
-        print(switcher.get(chosen_item))
-    
     def run(self) :
         tool_name = input("Type the name of the tool you are looking for: ")
         print("The information below contain: tool name, is it available?, price per day, price per half day, fine.")
         tool_controller = ToolController()
         tool_controller.findByName(tool_name)
-        chosen_item = input("Choose the item of your interest by typing a number: ")
-        self.caseStatement(chosen_item)
+
         
     
