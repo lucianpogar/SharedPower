45c682bc2f0623cec0ddc17bab10d7fe7f4838b8
mamaj20 <martynamaj@icloud.com>
2020-01-04 17:06:08 +0000

    updated the query with the use of LIKE operator

--- sharedpower/dao/ToolDao.py
+++ sharedpower/dao/ToolDao.py
@@ -4,7 +4,7 @@ class ToolDao:
     def findByName(self, tool_name):
         database_connection = DatabaseHelpers.createConnection()
         cursor = database_connection.cursor()
-        cursor.execute('SELECT tool_name, is_active, price_per_day, price_per_half_day, fine FROM TOOL WHERE tool_name = ?', (tool_name,))
+        cursor.execute("SELECT tool_name, is_active, price_per_day, price_per_half_day, fine FROM TOOL WHERE tool_name LIKE '%' || ? || '%'", (tool_name,))
         tool_row = cursor.fetchall()
         for count, i in enumerate(tool_row, start=1):
             print(count, ".", i)
