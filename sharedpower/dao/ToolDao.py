8c32780ccc6af6d5a2492c701f7aad88f5a4df4f
Martyna <58170503+mamaj20@users.noreply.github.com>
2020-01-06 19:49:54 +0000

    Search tools (#2)
    * added findByName implementation into ToolDao
    
    * added tool to database
    
    * added findByName implementation in ToolDao
    
    * added TOOL table with two rows for testing
    
    * findByName function added
    
    * fetching rows from the database in ToolDao added
    
    * added UI for SearchToolsUI
    
    * added numbers adequate to the number of fetched records
    
    * added UI for SearchToolsUI and case selection
    
    * updated the query with the use of LIKE operator
    
    * added a few records in tool table
    
    * deleted switch statement
    
    * created a database with no records

---
+++ sharedpower/dao/ToolDao.py
@@ -0,0 +1,12 @@
+from ..dbconnection.DatabaseHelpers import DatabaseHelpers
+
+class ToolDao:
+    def findByName(self, tool_name):
+        database_connection = DatabaseHelpers.createConnection()
+        cursor = database_connection.cursor()
+        cursor.execute("SELECT tool_name, is_active, price_per_day, price_per_half_day, fine FROM TOOL WHERE tool_name LIKE '%' || ? || '%'", (tool_name,))
+        tool_row = cursor.fetchall()
+        for count, i in enumerate(tool_row, start=1):
+            print(count, ".", i)
+        DatabaseHelpers.closeConnection(database_connection)
+        return tool_row
--- sharedpower/userinterface/SearchToolsUI.py
+++ sharedpower/userinterface/SearchToolsUI.py
@@ -1,3 +1,12 @@
+from ..controllers.ToolController import ToolController
+#from ..model.Specification import toolSpecification
+
 class SearchToolsUI :
     def run(self) :
-        pass
+        tool_name = input("Type the name of the tool you are looking for: ")
+        print("The information below contain: tool name, is it available?, price per day, price per half day, fine.")
+        tool_controller = ToolController()
+        tool_controller.findByName(tool_name)
+
+        
+    
